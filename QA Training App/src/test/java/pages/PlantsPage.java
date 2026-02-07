e3e3fa9ae38363fd402b80da0de3910d29db3fd9
Chathushi Kavindya <chathushikavindya09@gmail.com>
2026-02-07 20:00:08 +0530

    feat: Implement UI tests and page objects for plant creation and filtering functionalities.(4/6)

--- QA Training App/src/test/java/pages/PlantsPage.java
+++ QA Training App/src/test/java/pages/PlantsPage.java
@@ -50,8 +50,8 @@ public class PlantsPage extends PageObject {
         public static final Target PAGE_TITLE = Target.the("Page title")
                         .located(By.xpath("//h1 | //h2"));
 
-        public static final Target PLANT_NAME_ERROR = Target.the("Plant Name error")
-                        .located(By.xpath("//*[contains(text(), 'Plant Name is required')]"));
+        public static final Target PLANT_NAME_ERROR = Target.the("Plant name error")
+                        .located(By.xpath("//*[contains(text(), 'Plant name is required')]"));
 
         public static final Target PRICE_ERROR = Target.the("Price error")
                         .located(By.xpath("//*[contains(text(), 'Price is required')]"));
--- QA Training App/src/test/resources/features/ui/plant/plants_ui_read.feature
+++ QA Training App/src/test/resources/features/ui/plant/plants_ui_read.feature
@@ -6,8 +6,9 @@ Feature: Plant Management - UI Read Operations
   @UI @UI_Plant_Read_001 @215063V
   Scenario: Filter Plants by Name
     Given the user is logged in as an admin user
+    When the user navigates to the Plants page
     And multiple plants with different names exist
-    When the user enters "Rose" in the "Search plant" input box
+    And the user enters "Rose" in the "Search plant" input box
     And the user clicks the "Search" button
     Then the list updates to show only plants matching "Rose"
     And non-matching plants are hidden
